035c7a930053ee06de647e1cb43d9e3479b827a2
mrsimpson3000 <43422672+mrsimpson3000@users.noreply.github.com>
2020-04-13 18:24:46 -0400

    I have a bug that selects all when I click on one task.

--- todo/src/components/Todo.js
+++ todo/src/components/Todo.js
@@ -4,7 +4,9 @@ export default function Todo(props) {
   return (
     <div
       className={`item${props.item.completed ? " completed" : ""}`}
-      onClick={() => props.toggleItem(props.item.id)}
+      onClick={() =>
+        props.dispatch({ type: "TOGGLE_TASK", payload: props.item.id })
+      }
     >
       <p className='dumpsterJuice'>{props.item.task}</p>
     </div>
